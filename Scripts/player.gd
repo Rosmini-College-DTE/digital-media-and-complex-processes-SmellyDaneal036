a79ab118f58c86ae796865369701684378ebdedd
SmellyDaneal036 <18140@rosmini.school.nz>
2024-07-12 18:40:32 +1200

    Enemy + Player addjustments
    Created an enemy
    character movement adjustments

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,9 +18,25 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("Move.L", "Move.R")
+	
+	# Flip the Sprite 
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true 
+	
+	#Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+	
+	# Apply movement 
 	if direction:
 		velocity.x = direction * SPEED
 	else:
